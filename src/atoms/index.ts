319355e17df67c5b9b819cf242331fad6ca314cf
Yüksel Kapan <yukselkapan@gmail.com>
2025-10-16 16:44:40 +0300

    Remove all project files and configurations for a complete reset of the application.

--- src/atoms/index.ts
+++
@@ -1,3 +0,0 @@
-export { CvButton } from "./cv-button";
-export { CvContactLink } from "./cv-contact-link";
-export { CvProjectCard } from "./cv-project-card";
--- src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { Projects } from "./projects";
-
-export { Cv } from "./cv";
-
-export { AboutMe } from "./about-me";
-
-export { Header } from "./header";
